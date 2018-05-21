c6646b2333bc784784e68f4eaf57f56bc5db96e8
Hillebrand van de Groep <github@hillebrand.io>
2018-05-21 13:57:52 +0200

    added config

--- http.py
+++ http.py
@@ -3,6 +3,12 @@ import dvs
 import ovfiets
 from station import get_station
 import datetime
+import json
+
+with open('config.json') as f: # app version, name and stuff
+    config = json.load(f)
+
+global config
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -16,20 +22,20 @@ def search():
 
 @app.route("/train/today/<trein_nummer>")
 def trein_today(trein_nummer):
-  return render_template("trein.html", trein = dvs.train("https://dvs.ovdingen.nl", datetime.datetime.today().strftime('%Y-%m-%d'), trein_nummer, None, True))
+  return render_template("trein.html", trein = dvs.train("https://dvs.ovdingen.nl", datetime.datetime.today().strftime('%Y-%m-%d'), trein_nummer, None, True), config = config)
 
 @app.route("/train/today/<trein_nummer>/<station>")
 def trein_today_station(trein_nummer, station):
-  return render_template("trein.html", trein = dvs.train("https://dvs.ovdingen.nl", datetime.datetime.today().strftime('%Y-%m-%d'), trein_nummer, station, True), cur_station_meta = get_station(station, "https://stations.ovdingen.nl"))
+  return render_template("trein.html", trein = dvs.train("https://dvs.ovdingen.nl", datetime.datetime.today().strftime('%Y-%m-%d'), trein_nummer, station, True), cur_station_meta = get_station(station, "https://stations.ovdingen.nl"), config = config)
 
 @app.route("/drgl/<date>/<trein_nummer>")
 def drgl(date, trein_nummer):
-  return render_template("drgl.html", trein = dvs.train("https://dvs.ovdingen.nl", date, trein_nummer))
+  return render_template("drgl.html", trein = dvs.train("https://dvs.ovdingen.nl", date, trein_nummer), config = config)
 
 @app.route("/station/<station>/")
 def station(station):
-  return render_template("station.html", station = dvs.station("https://dvs.ovdingen.nl", station), ovfiets = ovfiets.station("https://ovfiets.ovdingen.nl/", station), meta = get_station(station, "https://stations.ovdingen.nl"))
+  return render_template("station.html", station = dvs.station("https://dvs.ovdingen.nl", station), ovfiets = ovfiets.station("https://ovfiets.ovdingen.nl/", station), meta = get_station(station, "https://stations.ovdingen.nl"), config = config)
 
 @app.route("/ovfiets/<pup>/")
 def ovfietspup(pup):
-  return render_template("ovfiets.html", ovfiets = ovfiets.pickuppoint("https://ovfiets.ovdingen.nl", pup))
+  return render_template("ovfiets.html", ovfiets = ovfiets.pickuppoint("https://ovfiets.ovdingen.nl", pup), config = config)
